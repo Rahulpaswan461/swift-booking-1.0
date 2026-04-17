372051bb55c54565dd25066dd1df1cd5062d5dd5
Rahul Paswan <rahulpaswan@aegiscovenant.com>
2026-04-17 23:08:28 +0530

    Added cors policy in the backend

--- Backend/index.js
+++ Backend/index.js
@@ -6,6 +6,7 @@ import DoctorRouter from "./routes/Doctor.js"
 import OtpRouter from "./routes/OtpRouter.js"
 import AdminRouter from "./routes/Admin.js"
 import { protectDoctor } from "./middleware/auth.js"
+import cors from "cors"
 
 
 const app = express()
@@ -19,6 +20,13 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json())
 // app.use(protectDoctor)
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://swift-booking-1-0.vercel.app/', 
+  ],
+  credentials: true
+}))
 
 app.get("/health", (req, res) => {
     return res.send("ok")
